b8738bce2848f145d26296a593928e1e16a0289e
PsyKo <psykogoko@gmail.com>
2015-03-31 22:17:27 +0200

    Unit updates

---
+++ org.goko.core/src/org/goko/core/config/EnumGokoUnit.java
@@ -0,0 +1,51 @@
+package org.goko.core.config;
+
+import org.apache.commons.lang3.StringUtils;
+import org.goko.core.common.exception.GkTechnicalException;
+import org.goko.core.common.measure.SI;
+import org.goko.core.common.measure.SIPrefix;
+import org.goko.core.common.measure.US;
+import org.goko.core.common.measure.quantity.Length;
+import org.goko.core.common.measure.units.Unit;
+
+/**
+ * Goko available units
+ *
+ * @author PsyKo
+ *
+ */
+public enum EnumGokoUnit {
+	MILLIMETERS("millimeters", SIPrefix.MILLI(SI.METRE)),
+	INCHES("inches", US.INCH);
+
+	String code;
+	Unit<Length> unit;
+
+	private EnumGokoUnit(String code, Unit<Length> unit) {
+		this.code = code;
+		this.unit = unit;
+	}
+
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	public static EnumGokoUnit getEnum(String code) throws GkTechnicalException{
+		for (EnumGokoUnit enumValue : values()) {
+			if(StringUtils.equals(code, enumValue.getCode())){
+				return enumValue;
+			}
+		}
+		throw new GkTechnicalException("Enum with code '"+code+"' does not exist.");
+	}
+
+	/**
+	 * @return the unit
+	 */
+	public Unit<Length> getUnit() {
+		return unit;
+	}
+}
