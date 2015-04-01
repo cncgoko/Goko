f9b319097f87192bbf1e349c0caf5577e39213c8
PsyKo <psykogoko@gmail.com>
2015-04-02 00:02:31 +0200

    DRO Empty screen update

--- org.goko.base.dro/ui/org/goko/base/dro/DisplayReadOut.java
+++ org.goko.base.dro/ui/org/goko/base/dro/DisplayReadOut.java
@@ -4,24 +4,36 @@ package org.goko.base.dro;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.eclipse.core.commands.Command;
+import org.eclipse.core.commands.Parameterization;
+import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.core.commands.common.NotDefinedException;
+import org.eclipse.e4.core.commands.ECommandService;
+import org.eclipse.e4.core.commands.EHandlerService;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.wb.swt.ResourceManager;
 import org.eclipse.wb.swt.SWTResourceManager;
 import org.goko.base.dro.controller.DisplayReadOutController;
 import org.goko.base.dro.controller.DisplayReadOutModel;
 import org.goko.common.GkUiComponent;
 import org.goko.core.common.exception.GkException;
+import org.goko.core.common.exception.GkTechnicalException;
 import org.goko.core.controller.bean.MachineValueDefinition;
 
 /**
@@ -33,6 +45,10 @@ import org.goko.core.controller.bean.MachineValueDefinition;
 public class DisplayReadOut extends GkUiComponent<DisplayReadOutController, DisplayReadOutModel>{
 	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
 	private Composite parentComposite;
+	@Inject
+	private ECommandService commandService;
+	@Inject
+	private EHandlerService handlerService;
 
 	@Inject
 	public DisplayReadOut(IEclipseContext context) {
@@ -54,9 +70,47 @@ public class DisplayReadOut extends GkUiComponent<DisplayReadOutController, Disp
 		parent.setLayout(new FillLayout(SWT.HORIZONTAL));
 
 		this.parentComposite = new Composite(parent, SWT.NONE);
-		createFields(parentComposite);
+		if(CollectionUtils.isEmpty(getDataModel().getObservedValuesDefinition())){
+			createEmptyPanel(parentComposite);
+		}else{
+			createFields(parentComposite);
+		}
 	}
 
+	private void createEmptyPanel(Composite parent){
+		parent.setLayout(new GridLayout(1, false));
+		Composite composite = new Composite(parent, SWT.WRAP);
+		composite.setLayout(new GridLayout(1, false));
+		composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		formToolkit.adapt(composite);
+		formToolkit.paintBordersFor(composite);
+
+		Label lblNoValueHere = new Label(composite, SWT.WRAP | SWT.CENTER);
+		lblNoValueHere.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+		formToolkit.adapt(lblNoValueHere, true, true);
+		lblNoValueHere.setText("No value here ? You can configure the displayed values in the preferences.");
+
+		Button btnPreferences = new Button(composite, SWT.NONE);
+		btnPreferences.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+		btnPreferences.setImage(ResourceManager.getPluginImage("org.goko.base.dro", "icons/gear.png"));
+		formToolkit.adapt(btnPreferences, true, true);
+		btnPreferences.setText("Preferences");
+		btnPreferences.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent e) {
+				try {
+					Command cmd = commandService.getCommand("org.goko.base.dro.command.dropreferences");
+					Parameterization param = new Parameterization(cmd.getParameter("goko.org.ui.page.id"), "org.goko.dro.displayPreferences");
+					ParameterizedCommand pCmd = new ParameterizedCommand(cmd, new Parameterization[]{param});
+					if (handlerService.canExecute(pCmd)) {
+						handlerService.executeHandler(pCmd);
+					}
+				} catch (NotDefinedException e1) {
+					displayError(new GkTechnicalException(e1));
+				}
+			}
+		});
+	}
 	private void createFields(Composite composite){
 		composite.setBackground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_HIGHLIGHT_SHADOW));
 		composite.setLayout(new GridLayout(2, false));
@@ -90,7 +144,7 @@ public class DisplayReadOut extends GkUiComponent<DisplayReadOutController, Disp
 			Composite parent = parentComposite.getParent();
 			parentComposite.dispose();
 			parentComposite = new Composite(parent, SWT.NONE);
-			createFields(parentComposite);
+			createControls(parentComposite);
 			parent.setSize(parent.getSize());
 //			parent.redraw();
 //			parent.update();
