154cb77e0218b41a63427e527ac39f5d2eb39330
PsyKo <psykogoko@gmail.com>
2015-04-01 23:27:50 +0200

    Measure tests

---
+++ org.goko.core/junit/org/goko/core/common/measure/MeasureTest.java
@@ -0,0 +1,33 @@
+/*
+ *	This file is part of Goko.
+ *
+ *  Goko is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Goko is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Goko.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.goko.core.common.measure;
+
+
+public class MeasureTest {
+
+	public static void main(String[] args) {
+//		Quantity<Length> l = NumberQuantity.of(1.0, INCH);
+//		System.err.println(l.to(MILLI(METRE)).doubleValue()+" "+MILLI(METRE).getSymbol());
+//		System.err.println(l.to(NANO(METRE)).to(FOOT).doubleValue()+" "+FOOT.getSymbol());
+//		System.err.println(l.to(MILLI(METRE)).to(INCH).doubleValue()+" "+INCH.getSymbol());
+//
+//
+//		System.err.println("1 µm = "+NumberQuantity.of(1, MICRO(METRE)).to(METRE).doubleValue()+" m");
+
+	}
+}
