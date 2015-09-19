9be60690f83cf1d94df6b4a9c717dcbc976ce449
PsyKo <contact@goko.fr>
2015-09-19 17:25:03 +0200

    Fix Grbl Check mode button

--- org.goko.controller.grbl.v08/ui/org/goko/controller/grbl/v08/internal/handlers/GrblCheckModeHandler.java
+++ org.goko.controller.grbl.v08/ui/org/goko/controller/grbl/v08/internal/handlers/GrblCheckModeHandler.java
@@ -3,7 +3,7 @@ package org.goko.controller.grbl.v08.internal.handlers;
 import org.apache.commons.lang3.ObjectUtils;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.ui.model.application.ui.menu.MHandledToolItem;
+import org.eclipse.e4.ui.model.application.ui.menu.MHandledMenuItem;
 import org.goko.controller.grbl.v08.GrblMachineState;
 import org.goko.controller.grbl.v08.IGrblControllerService;
 import org.goko.core.common.exception.GkException;
@@ -11,9 +11,9 @@ import org.goko.core.log.GkLog;
 
 public class GrblCheckModeHandler {
 	private static final GkLog LOG = GkLog.getLogger(GrblCheckModeHandler.class);
-	
+		
 	@CanExecute
-	public boolean canExecute(MHandledToolItem menuItem, IGrblControllerService controllerService){
+	public boolean canExecute(MHandledMenuItem menuItem, IGrblControllerService controllerService){
 		try {
 			menuItem.setSelected(ObjectUtils.equals( GrblMachineState.CHECK, controllerService.getState()));	
 						
@@ -25,7 +25,7 @@ public class GrblCheckModeHandler {
 	}
 	
 	@Execute
-	public void execute(MHandledToolItem menuItem, IGrblControllerService service) throws GkException {
+	public void execute(MHandledMenuItem menuItem, IGrblControllerService service) throws GkException {
 		if(ObjectUtils.equals( GrblMachineState.CHECK, service.getState())){
 			service.setCheckModeEnabled(false);
 			menuItem.setSelected(false);
