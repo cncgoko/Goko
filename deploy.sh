cf9aba00fe9f54f8e5399b42ea25f3f013bc9012
cncgoko <cncgoko@users.noreply.github.com>
2015-09-22 00:10:40 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -e # exit with nonzero exit code if anything fails
-
-cd $HOME
+echo "Exporting binaries..."
 
 curl --ftp-create-dirs -T $HOME/org.goko.build.product/target/products/org.goko-win32.win32.x86_64.zip -u $VAR1:$VAR2 $TARGET/org.goko-win32.win32.x86_64.zip
