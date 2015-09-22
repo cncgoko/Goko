3da7834b0d006fdca611a790751c0b82f7337986
cncgoko <cncgoko@users.noreply.github.com>
2015-09-22 14:02:34 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 cd /home/travis/build/cncgoko/Goko/org.goko.build.product/target/repository/
 find . -type f -exec echo {} {} \;
+
 if [ $updateRepository == 'true' ]
 then
-  echo "Exporting repository..."
-  # TODO change target to remove hardcoded 'nightly'
-  find . -type f -exec curl --ftp-create-dirs -T {} -u $VAR1:$VAR2 $TARGET/$gokoVersion/{} \;
+  cleanRepository();  
+  exportRepository();  
 else
   echo "Skipped repository export..."
 fi
-#curl --ftp-create-dirs -T $HOME/org.goko.build.product/target/products/org.goko-win32.win32.x86_64.zip -u $VAR1:$VAR2 $TARGET/org.goko-win32.win32.x86_64.zip
+
+cleanRepository(){
+  echo "Cleaning repository..."
+  find . -type f -exec curl -u $VAR1:$VAR2 $TARGET/$gokoVersion/ -Q "DELE {}" \;
+}
+
+exportRepository(){
+  echo "Exporting repository..."
+  # TODO change target to remove hardcoded 'nightly'
+  find . -type f -exec curl --ftp-create-dirs -T {} -u $VAR1:$VAR2 $TARGET/$gokoVersion/{} \;
+}
