a7bea2846fd96dca9b8bd5dbe6c285219fe38795
cncgoko <cncgoko@users.noreply.github.com>
2015-09-22 16:40:45 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,10 @@
 # Clean the distant repository
 cleanRepository(){
   echo "Cleaning repository..."
-  curl -u $VAR1:$VAR2 $TARGET/ -Q "RMD 0.0.2/"
+#  curl -u $VAR1:$VAR2 $TARGET/ -Q "RMD 0.0.2/"
+  curl -u $VAR1:$VAR2 $TARGET/ -Q "MKD 0.0.3/"
+  curl -u $VAR1:$VAR2 $TARGET/ -Q "MKD 0.0.4/"
+  curl -u $VAR1:$VAR2 $TARGET/ -Q "RMD 0.0.3/"
 }
 
 # Clean the distant repository
