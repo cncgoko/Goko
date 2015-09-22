00b1039e8d93276a43c919eab6bc8f7f6d2812cc
cncgoko <cncgoko@users.noreply.github.com>
2015-09-22 14:17:25 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
-echo $TRAVIS_BUILD_DIR
 
+# Clean the distant repository
+cleanRepository(){
+  echo "Cleaning repository..."
+  curl -u $VAR1:$VAR2 $TARGET/$gokoVersion/ -Q "mdelete *" \;
+}
 
+# Clean the distant repository
+exportRepository(){
+  echo "Exporting repository..."
+  # Switch to repository build location
+  cd $TRAVIS_BUILD_DIR/org.goko.build.product/target/repository/
+  # TODO change target to remove hardcoded 'nightly'
+  find . -type f -exec curl --ftp-create-dirs -T {} -u $VAR1:$VAR2 $TARGET/$gokoVersion/{} \;
+}
+
+# Let's do it
+if [ $updateRepository == 'true' ]
+then
+  cleanRepository  
+  exportRepository  
+else
+  echo "Skipped repository export..."
+fi
