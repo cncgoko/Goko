667cfa5adbe17bc2fa69d6a71a8cb7a5a5de977e
cncgoko <cncgoko@users.noreply.github.com>
2015-09-22 10:21:15 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ echo "Exporting binaries..."
 
 cd /home/travis/build/cncgoko/Goko/org.goko.build.product/target/repository/
 
-"find . -type f -exec curl --ftp-create-dirs -T {} -u $VAR1:$VAR2 $TARGET/"
+find . -type f -exec curl --ftp-create-dirs -T {} -u $VAR1:$VAR2 $TARGET/
 
 #curl --ftp-create-dirs -T $HOME/org.goko.build.product/target/products/org.goko-win32.win32.x86_64.zip -u $VAR1:$VAR2 $TARGET/org.goko-win32.win32.x86_64.zip
