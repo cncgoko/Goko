77a152cbffcf043f2db14e91b01f7cec17972eb4
cncgoko <cncgoko@users.noreply.github.com>
2015-09-22 12:07:58 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 cd /home/travis/build/cncgoko/Goko/org.goko.build.product/target/repository/
 find . -type f -exec echo {} {} \;
-if [$updateRepository == 'true'] then
+if [ $updateRepository == 'true' ]
+then
   echo "Exporting repository..."
   
   find . -type f -exec curl --ftp-create-dirs -T {} -u $VAR1:$VAR2 $TARGET/{} \;
+else
+  echo "Skipped repository export..."
 fi
 #curl --ftp-create-dirs -T $HOME/org.goko.build.product/target/products/org.goko-win32.win32.x86_64.zip -u $VAR1:$VAR2 $TARGET/org.goko-win32.win32.x86_64.zip
