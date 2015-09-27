485c6b847f6fc2d32f94312571412265e735427b
cncgoko <cncgoko@users.noreply.github.com>
2015-09-27 19:34:36 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ exportRepository(){
 }
 
 # Export the built binaries to destination
-exportRepository(){
+exportBinaries(){
   echo "Exporting binaries..."
   
   cd $TRAVIS_BUILD_DIR/org.goko.build.product/target/products/
