a71be8c8038d506a501c35cb3c5d1bbdd9da1130
Psyko <contact@goko.fr>
2016-12-18 00:33:19 +0100

    About dialog changes

--- Goko/src/goko/handlers/AboutDialog.java
+++ Goko/src/goko/handlers/AboutDialog.java
@@ -1,10 +1,14 @@
 package goko.handlers;
 
+import java.awt.Desktop;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.util.Properties;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -54,7 +58,7 @@ public class AboutDialog extends Dialog {
 	 */
 	private void createContents() {
 		shlAboutGoko = new Shell(getParent(), getStyle());
-		shlAboutGoko.setSize(376, 148);
+		shlAboutGoko.setSize(376, 248);
 		shlAboutGoko.setText("About Goko");
 		shlAboutGoko.setLayout(new GridLayout(1, false));
 
@@ -107,10 +111,76 @@ public class AboutDialog extends Dialog {
 		composite.setLayout(new GridLayout(2, false));
 
 		Label lblMoreInformationOn = new Label(composite, SWT.NONE);
-		lblMoreInformationOn.setText("More information on");
+		GridData gd_lblMoreInformationOn = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_lblMoreInformationOn.widthHint = 60;
+		lblMoreInformationOn.setLayoutData(gd_lblMoreInformationOn);
+		lblMoreInformationOn.setText("Website :");
 
 		Link link = new Link(composite, SWT.NONE);
+		link.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent event) {
+				if (event.button == 1) {   // Left button pressed & released
+		            Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+		            if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+		                try {
+		                    desktop.browse(URI.create("http://www.goko.fr"));
+		                } catch (Exception e) {
+		                    LOG.error(e);
+		                }
+		            }
+		        }
+			}
+		});
 		link.setText("<a>http://www.goko.fr</a>");
+		
+		Composite composite_3 = new Composite(composite_1, SWT.NONE);
+		composite_3.setLayout(new GridLayout(2, false));
+		
+		Label lblForum = new Label(composite_3, SWT.NONE);
+		GridData gd_lblForum = new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1);
+		gd_lblForum.widthHint = 60;
+		lblForum.setLayoutData(gd_lblForum);
+		lblForum.setText("Forum :");
+		
+		Link link_1 = new Link(composite_3, 0);
+		link_1.setText("<a>http://discuss.goko.fr</a>");
+		link_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent event) {
+				if (event.button == 1) {   // Left button pressed & released
+		            Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+		            if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+		                try {
+		                    desktop.browse(URI.create("http://discuss.goko.fr"));
+		                } catch (Exception e) {
+		                    LOG.error(e);
+		                }
+		            }
+		        }
+			}
+		});
+		
+		Composite composite_4 = new Composite(composite_1, SWT.NONE);
+		composite_4.setLayout(new GridLayout(2, false));
+		
+		Label lblContact = new Label(composite_4, SWT.NONE);
+		GridData gd_lblContact = new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1);
+		gd_lblContact.widthHint = 60;
+		lblContact.setLayoutData(gd_lblContact);
+		lblContact.setText("Contact :");
+			    
+		Link link_2 = new Link(composite_4, 0);
+		link_2.setText("<a>"+toAscii("636f6e7461637440676f6b6f2e6672")+"</a>");
 
+	}
+	
+	private String toAscii(String hex){
+	    StringBuilder output = new StringBuilder();
+	    for (int i = 0; i < hex.length(); i+=2) {
+	        String str = hex.substring(i, i+2);
+	        output.append((char)Integer.parseInt(str, 16));
+	    }
+	    return output.toString();
 	}
 }
